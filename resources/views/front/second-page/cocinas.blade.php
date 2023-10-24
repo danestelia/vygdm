bdff3da77ca74851e840d4f401c557cbc6765d1a
unknown <daneste000@hotmail.com>
2023-10-23 21:26:08 -0500

    Se agrega página cocinas.blade.php
    Se agrega página de información de cocinas integrales con sus respectivas imágenes,
    y se modifica el archivo app.css con atributos para dicha página.

--- resources/views/front/second-page/cocinas.blade.php
+++ resources/views/front/second-page/cocinas.blade.php
@@ -6,6 +6,81 @@
     </div>
   </div>
 </section>
+<section class="vyg-mt-70 vyg-mb-60">
+  <div class="container">
+    <h1 class="uk-heading-divider">Cocinas Integrales Barranquilla: Diseño y Funcionalidad</h1>
+    <br>
+    <div class="uk-panel">
+      <img class="uk-align-left uk-margin-remove-adjacent" src="{{ asset('img/cocina-integral-page.jpg') }}" width="400"
+           height="250" alt="Cocina integral Barranquilla">
+      <p class="text-justify">En <strong>V & G Diseño Mobiliario S.A.S</strong>, entendemos que la cocina es el corazón
+        del hogar. Es el lugar
+        donde se preparan
+        deliciosas comidas, se comparten momentos especiales y se crean recuerdos duraderos. Por eso, ofrecemos las
+        mejores cocinas integrales Barranquilla, diseñadas para combinar estilo y funcionalidad. En este artículo,
+        exploraremos por qué nuestras cocinas integrales son la elección perfecta para su hogar.</p>
+      <h3 class="text-left">Diseños Personalizados</h3>
+      <p class="text-justify">Nuestras cocinas integrales se destacan por su diseño personalizado. Entendemos que cada
+        hogar es único, y por eso trabajamos estrechamente con nuestros clientes para crear cocinas que se adapten a sus
+        necesidades y gustos. Ya sea que prefiera un diseño moderno y minimalista o uno más clásico y acogedor, nuestro
+        diseñador le ayudará a crear la cocina de sus sueños.</p>
+      <h3 class="text-left">Materiales de Calidad</h3>
+      <p class="text-justify">En <strong>V & G Diseño Mobiliario S.A.S</strong>, utilizamos solo los materiales de alta
+        calidad en la
+        fabricación de nuestras
+        cocinas integrales Barranquilla. Esto garantiza la durabilidad y la longevidad de nuestros productos. Nuestras
+        cocinas están
+        construidas para resistir el desgaste diario y lucir hermosas durante muchos años.</p>
+    </div>
+    <div class="uk-panel">
+      <img class="uk-align-right uk-margin-remove-adjacent" src="{{ asset('img/melamina-rh.png') }}" width="280"
+           height="150" alt="Melamina RH">
+      <p class="text-justify">Debido a la alta humedad existente en la ciudad de Barranquilla, para la construcción de
+        nuestras cocinas integrales, utilizamos solo la melamina RH (Resistente a la humedad ambiente), el cual es un
+        material hecho a base de
+        viruta de madera de pino de alta calidad, el cual es inpregnado con formaldehido, que es un pegante de alto
+        rendimiento que le da al material
+        la resistencia a la humedad ambiente, y que a la ves sirve como un repelente para insectos como la polilla y el
+        comején, el material es de alta resistencia
+        al desgaste y al rayado.</p>
+      <p class="text-justify">En cuanto a herrajes, los hay de gama baja, gama media y gama alta, en <strong>V & G
+          Diseño
+          Mobiliario S.A.S</strong> utilizamos para nuestras cocinas integrales Barranquilla, herrajes de calidad gama media o superior, dependiendo ésto de el
+        presupuesto
+        de nuesros clientes, pudiendo así garantizar la duración de nuestros proyectos</p>
+      <h3 class="text-left">Funcionalidad Inteligente</h3>
+      <p class="text-justify">Una cocina integral debe ser funcional, y entendemos la importancia de cada detalle.
+        Nuestras cocinas están diseñadas pensando en la comodidad y la eficiencia. Ofrecemos soluciones inteligentes de
+        almacenamiento, encimeras espaciosas y distribuciones que facilitan la preparación de alimentos. Todo esto se
+        combina para crear un espacio de cocina que realmente funcione para usted.</p>
+      <h3 class="text-left">Sostenibilidad Ambiental</h3>
+      <p class="text-justify">En <strong>V & G Diseño Mobiliario</strong>, nos preocupamos por el medio ambiente. Es por
+        eso que nos
+        esforzamos por utilizar materiales sostenibles en la fabricación de nuestras cocinas integrales. Esto no solo
+        beneficia al planeta, sino que también garantiza que su cocina sea segura para su familia. </p>
+      <p class="text-justify">Nuestros proveedores de melaminas son poseedores de bosques autosustentables, los cuales son reforestados
+        para ayudar
+        a conservar el medio ambiente y detener un tanto el cambio climático.</p>
+      <h3 class="text-left">Garantía de Satisfacción</h3>
+      <p class="text-justify">Respaldamos la calidad de nuestras cocinas integrales Barranquilla con una sólida garantía
+        de
+        satisfacción. Estamos comprometidos a superar las expectativas de nuestros clientes en cada proyecto que
+        emprendemos. Así como tambien nos esmeramos por entregar nuestros proyectos en el tiempo determinado al
+        contratar nuestro servicio.</p>
+      <br>
+      <p class="text-justify">Si está buscando cocinas integrales en Barranquilla que combinen diseño excepcional y
+        funcionalidad inteligente, <strong>V & G Diseño Mobiliario S.A.S</strong> es su elección ideal. Contáctenos hoy mismo para comenzar a
+        diseñar la cocina de sus sueños. Nuestro equipo de expertos está listo para convertir su visión en realidad.</p>
+    </div>
+  </div>
+  <div class="btn-action pb-50">
+    <a class="cotiza waves-effect waves-light btn-large"><i class="tiny material-icons left">event_note</i>Cotiza tu
+      proyecto aquí</a>
+    <br>
+    <a class="llama waves-effect waves-light btn-large"><i class="material-icons left">call</i>Contáctanos ahora</a>
+  </div>
+
+</section>
 
 
 <livewire:front.piecera/>
